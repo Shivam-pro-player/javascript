06f578d1b2fc36a8284293b1b7c09c15a20bcaf0
Shivam-pro-player <shivamyadav18496@gmail.com>
2024-10-22 06:35:40 +0000

    datatypes

---
+++ .devcontainer/datatypes.js
@@ -0,0 +1,18 @@
+"use strict";  //treates all the js code in the newer version of js
+//alerrt ( 2+3 )//we r not using browser, we r using node.js
+console.log(2+2)
+console.log("shivam")
+//code reeadeability should be high
+let name = "shivam"
+let age = 20
+let isloggedin = false
+
+//number = 2 to the power 53
+//bigint
+//string = ""
+//boolean = true/false
+//null = stand alone value
+//undefined
+//symbol = unique
+//object
+console.log (typeof "shivam")
