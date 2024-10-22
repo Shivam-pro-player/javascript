f1b511821e88413ed47909cf6fc13e7392b3f583
Shivam-pro-player <shivamyadav18496@gmail.com>
2024-10-22 08:41:04 +0000

    conversionoperation

---
+++ .devcontainer/conversionoperation.js
@@ -0,0 +1,21 @@
+let score = true
+//console.log(typeof (score))
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+//NaN meanns not a number
+//"33" => 33
+//"33abc" => nan
+//true => 1
+//false => 0
+let isloggedin = 1
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin)
+//1 = true
+//0 = false
+// "" = false
+//"shivam" = true
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
