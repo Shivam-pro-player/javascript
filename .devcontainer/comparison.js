82f9532b3f7b7261ac85bdd946b329c9f9ea5a6d
Shivam-pro-player <shivamyadav18496@gmail.com>
2024-10-23 05:43:52 +0000

    comparison

---
+++ .devcontainer/comparison.js
@@ -0,0 +1,18 @@
+// console.log ("2" > 1)
+//console.log("02" > 1)
+
+console.log(null > 0);
+console.log(null == 0);
+
+console.log(null >= 0);
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// avoid these types of comparison
+
+// ===  //this o/does not convert the datatype of "2"
+console.log("2" === 2);
+
+
+
+
